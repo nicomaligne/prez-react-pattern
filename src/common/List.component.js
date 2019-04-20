a7a46c6b82e653140e33bd837fa500c39a9953a8
Jimmy Somsanith <jsomsanith@talend.com>
2019-04-20 16:12:31 +0200

    Step 1

--- src/common/List.component.js
+++ src/common/List.component.js
@@ -1,58 +1,86 @@
-import React from "react";
-import "./List.scss";
+import React from 'react';
+import PropTypes from 'prop-types';
+import './List.scss';
 
-function Value({ header, item, nameKey }) {
+function Value({ header, item, name }) {
   if (!header) {
     return;
   }
   switch (header.type) {
-    case "image":
-      return <img src={item[header.key]} alt={item[nameKey]} />;
+    case 'image':
+      return <img src={item[header.key]} alt={name} />;
     default:
       return item[header.key];
   }
 }
+Value.propTypes = {
+  header: PropTypes.shape({
+    key: PropTypes.string,
+    type: PropTypes.string
+  }),
+  item: PropTypes.object,
+  name: PropTypes.string
+};
 
 function TableRow({ headers, item }) {
-  const nameKey = headers.find(({ type }) => type === "main").key;
+  const nameKey = headers.find(({ type }) => type === 'main').key;
   const name = item[nameKey];
   return (
     <tr>
       {headers.map(header => (
-        <td className={`col-${header.type || "text"}`}>
+        <td key={header.key} className={`col-${header.type || 'text'}`}>
           <Value header={header} item={item} name={name} />
         </td>
       ))}
     </tr>
   );
 }
+TableRow.propTypes = {
+  header: PropTypes.shape({
+    key: PropTypes.string,
+    type: PropTypes.string
+  }),
+  item: PropTypes.object
+};
 
 function Table(props) {
-  const { title, headers, collection } = props;
+  const { collection, headers, title } = props;
   return (
     <table className="list list-table">
       <caption className="sr-only">{title}</caption>
       <thead>
         <tr>
           {headers.map(header => (
-            <th className={`col-${header.type || "text"}`}>{header.label}</th>
+            <th key={header.key} className={`col-${header.type || 'text'}`}>
+              {header.label}
+            </th>
           ))}
         </tr>
       </thead>
       <tbody>
-        {collection.map(item => (
-          <TableRow headers={headers} item={item} />
+        {collection.map((item, index) => (
+          <TableRow key={index} headers={headers} item={item} />
         ))}
       </tbody>
     </table>
   );
 }
+TableRow.propTypes = {
+  collection: PropTypes.arrayOf(PropTypes.object),
+  headers: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      type: PropTypes.string
+    })
+  ),
+  title: PropTypes.string
+};
 
 function Large(props) {
-  const { title, headers, collection } = props;
-  const idHeader = headers.find(({ type }) => type === "id");
-  const mainHeader = headers.find(({ type }) => type === "main");
-  const imageHeader = headers.find(({ type }) => type === "image");
+  const { collection, headers, title } = props;
+  const idHeader = headers.find(({ type }) => type === 'id');
+  const mainHeader = headers.find(({ type }) => type === 'main');
+  const imageHeader = headers.find(({ type }) => type === 'image');
   const otherHeaders = headers.filter(
     header =>
       header !== idHeader && header !== mainHeader && header !== imageHeader
@@ -62,8 +90,8 @@ function Large(props) {
     <div className="list list-large">
       <h1 className="sr-only">{title}</h1>
       <ul>
-        {collection.map(item => (
-          <li>
+        {collection.map((item, index) => (
+          <li key={index}>
             <div className="list-large-content">
               <h2>
                 <Value
@@ -80,7 +108,7 @@ function Large(props) {
               </h2>
               <dl>
                 {otherHeaders.map(header => (
-                  <div className="list-large-keyvalue">
+                  <div key={header.key} className="list-large-keyvalue">
                     <dt>{header.label}</dt>
                     <dd>
                       <Value
@@ -104,13 +132,28 @@ function Large(props) {
     </div>
   );
 }
+TableRow.propTypes = {
+  collection: PropTypes.arrayOf(PropTypes.object),
+  headers: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      type: PropTypes.string
+    })
+  ),
+  title: PropTypes.string
+};
 
 export default function List(props) {
-  const { displayMode = "table", ...rest } = props;
+  const { displayMode = 'table', ...rest } = props;
   switch (displayMode) {
-    case "table":
+    case 'table':
       return <Table {...rest} />;
-    case "large":
+    case 'large':
       return <Large {...rest} />;
+    default:
+      return <div>Unknown display mode "{displayMode}"</div>;
   }
 }
+List.propTypes = {
+  displayMode: PropTypes.string
+};
