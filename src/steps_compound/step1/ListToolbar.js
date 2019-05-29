d06a4653d52723880fe6550fa57eb4cb6336371a
nicomaligne <nmaligne@talend.com>
2019-05-29 11:02:56 +0200

    remove comment

--- src/steps_compound/step1/ListToolbar.js
+++ src/steps_compound/step1/ListToolbar.js
@@ -2,10 +2,6 @@ import React from 'react';
 import '../../common/toolbar.scss';
 import { ListToolbarProptypes } from './propTypes';
 
-/*
-    Add a default render ?
-*/
-
 const ListToolbar = ({ children }) => <nav className="toolbar">{children}</nav>;
 
 ListToolbar.propTypes = ListToolbarProptypes;
