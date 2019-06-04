8d9d8c22a98121ec749e60b334fc2a508a1e5ff2
nicomaligne <nmaligne@talend.com>
2019-06-04 07:51:11 +0200

    remove console log

--- src/steps_compound/step3/ToolbarDisplayMode.js
+++ src/steps_compound/step3/ToolbarDisplayMode.js
@@ -14,7 +14,6 @@ const ToolbarDisplayMode = ({
   } = useContextCompoundList();
 
   useEffect(() => {
-    console.log('toto')
     if (!displayModeValue && !displayModeContext) {
       setDisplayModeContext(initialDisplayMode);
     }
