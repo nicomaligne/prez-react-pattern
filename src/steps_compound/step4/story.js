a2c7e7c0c6dc6bf5d033f80ea25813fdbd2a576e
nicomaligne <nmaligne@talend.com>
2019-06-12 19:17:50 +0200

    remove controlled behavior

--- src/steps_compound/step4/story.js
+++ src/steps_compound/step4/story.js
@@ -6,40 +6,23 @@ import { headers, actions } from '../../data/list-config';
 
 import List from './index';
 
-const ExternalLogicalDisplayMode = ({ children }) => {
-  const [displayModeValue, setDisplayModeValue] = useState('large');
-  const onChangeDisplayMode = value => {
-    console.log('this is my custon onChange display mode');
-    setDisplayModeValue(value);
-  };
-  return (
-    <React.Fragment>
-      {children(displayModeValue, onChangeDisplayMode)}
-    </React.Fragment>
-  );
-};
-
 export const Step4ControlledDisplayMode = () => {
   return (
-    <ExternalLogicalDisplayMode>
-      {(displayModeValue, onChangeDisplayMode) => (
-        <List.Manager id={'myContextId'}>
-          <List.Toolbar>
-            <List.SelectAll onSelectAll={action('onSelectAll')} />
-            <List.Actions actions={actions} />
-            <List.DisplayMode
-              value={displayModeValue}
-              onChange={onChangeDisplayMode}
-            />
-          </List.Toolbar>
-          <List.List
-            title="datasets"
-            headers={headers}
-            collection={pokemons}
-            displayModeValue={displayModeValue}
-          />
-        </List.Manager>
-      )}
-    </ExternalLogicalDisplayMode>
+    <List.Manager id={'myContextId'}>
+      <List.Toolbar>
+        <List.SelectAll onSelectAll={action('onSelectAll')} />
+        <List.Actions actions={actions} />
+        <List.DisplayMode
+          onChange={action('this is my custon onChange display mode')}
+          value="table"
+        />
+      </List.Toolbar>
+      <List.List
+        collection={pokemons}
+        displayModeValue="table"
+        headers={headers}
+        title="datasets"
+      />
+    </List.Manager>
   );
 };
